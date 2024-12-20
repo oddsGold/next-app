fb4a4249bbdcfcdacddcea4437170eab4ee5f6ef
Sulym Dmytro <ds.intelserv@gmail.com>
2024-12-20 13:46:46 +0100

    add search

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {cn} from "@/lib/utils";
-import {Container} from "@/components/shared";
+import {Container, SearchInput} from "@/components/shared";
 import Image from "next/image";
 import {Button} from "@/components/ui";
 import {ArrowRight, ShoppingCart, User} from "lucide-react";
+import Link from "next/link";
 
 interface Props {
     className?: string;
@@ -12,18 +13,24 @@ interface Props {
 export const Header: React.FC<Props> = ({ className }) => {
  return (
   <header className={cn('border border-b', className)}>
-      <Container className='flex items-center justify-between py-8'>
-          <div className='flex items-center gap-4'>
-              <Image src="/logo.png" alt="Logo" width={35} height={35} />
-              <div>
-                  <h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
-                  <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+      <Container className='flex items-center justify-between py-8'>\
+          <Link href="/">
+              <div className='flex items-center gap-4'>
+                  <Image src="/logo.png" alt="Logo" width={35} height={35}/>
+                  <div>
+                      <h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
+                      <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                  </div>
               </div>
+          </Link>
+
+          <div className="mx-10 flex-1">
+              <SearchInput />
           </div>
 
           <div className='flex items-center gap-3'>
               <Button variant="outline" className="flex items-center gap-1">
-                  <User size={16} />
+                  <User size={16}/>
                   Войти
               </Button>
 
---
+++ services/products.ts
@@ -0,0 +1,9 @@
+import {Product} from "@prisma/client";
+import {instance} from "./instance";
+import {ApiRoutes} from "@/services/constants";
+
+export const search = async (query: string): Promise<Product[]> => {
+    const { data } = await instance.get<Product[]>(ApiRoutes.SEARCH_PRODUCTS, {params: {query}});
+
+    return data;
+}
