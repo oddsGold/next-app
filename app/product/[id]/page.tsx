b825fc07a80289058af3f46a4f5e2b21eb8102c2
Sulym Dmytro <ds.intelserv@gmail.com>
2025-01-14 19:17:49 +0100

    update current product page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,5 +1,46 @@
-export default function ProductPage({params: { id }} : { params: { id: string } }) {
+import {prisma} from "@/prisma/prisma-client";
+import {notFound} from "next/navigation";
+import {Container, ProductImage, Title} from "@/components/shared";
+import {GroupVariants} from "@/components/shared/group-variants";
+
+export default async function ProductPage({params: {id}}: { params: { id: string } }) {
+
+    const product = await prisma.product.findFirst({where: {id: Number(id)}});
+
+    if (!product) {
+        return notFound();
+    }
+
     return (
-        <div>{id}</div>
+        <Container className="flex flex-col my-10">
+            <div className="flex flex-1">
+                <ProductImage imageUrl={product.imageUrl} size={40}/>
+
+                <div className="w-[490px] bg-[#FCFCFC] p-7">
+                    <Title text={product.name} size="md" className="font-extrabold mb-1"/>
+
+                    <p className="text-gray-400">Lorem ipsum dolor sit amet, consectetur.</p>
+
+                    <GroupVariants selectedValue="2"
+                                   items={
+                                       [
+                                           {
+                                               name: 'Small',
+                                               value: '1'
+                                           },
+                                           {
+                                               name: 'Middle',
+                                               value: '2'
+                                           },
+                                           {
+                                               name: 'Big',
+                                               value: '3',
+                                               disabled: true
+                                           }
+                                       ]
+                                   }/>
+                </div>
+            </div>
+        </Container>
     )
 }
