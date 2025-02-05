10c73a57b64a28e83730194b193cc983b6a55979
Sulym Dmytro <ds.intelserv@gmail.com>
2025-02-05 19:41:42 +0100

    Added the ability to add products to the cart

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import {Container, Filters, Title, TopBar} from "@/shared/components/shared";
 import {ProductGroupList} from "@/shared/components/shared/products-group-list";
 import {prisma} from "@/prisma/prisma-client";
+import {Suspense} from "react";
 
 export default async function Home() {
     const categories = await prisma.category.findMany({
@@ -25,7 +26,7 @@ export default async function Home() {
             <Container className="mt-10 pb-14">
                 <div className="flex gap-[80px]">
                     <div className="w-[250px]">
-                        <Filters/>
+                        <Suspense><Filters/></Suspense>
                     </div>
 
                     <div className="flex-1">
--- shared/components/shared/choose-pizza-form.tsx
+++ shared/components/shared/choose-pizza-form.tsx
@@ -1,47 +1,49 @@
 'use client';
 
 import React from 'react';
-import {cn} from "@/shared/lib/utils";
-import {PizzaImage} from "@/shared/components/shared/pizza-image";
-import {Title} from "@/shared/components/shared/title";
-import {Button} from "@/shared/components/ui";
-import {GroupVariants} from "@/shared/components/shared/group-variants";
-import { PizzaSize, PizzaType, pizzaTypes} from "@/shared/constants/pizza";
-import {Ingredient, ProductItem} from "@prisma/client";
-import {IngredientItem} from "@/shared/components/shared/ingredient-item";
-import {usePizzaOptions} from "@/shared/hooks";
-import {getPizzaDetails} from "@/shared/lib";
+import { Ingredient, ProductItem } from '@prisma/client';
+
+import { PizzaImage } from './pizza-image';
+import { Title } from './title';
+import { Button } from '../ui';
+import { GroupVariants } from './group-variants';
+import { PizzaSize, PizzaType, pizzaTypes } from '@/shared/constants/pizza';
+import { IngredientItem } from './ingredient-item';
+import { cn } from '@/shared/lib/utils';
+import { getPizzaDetails } from '@/shared/lib';
+import { usePizzaOptions } from '@/shared/hooks';
 
 interface Props {
     imageUrl: string;
     name: string;
     ingredients: Ingredient[];
     items: ProductItem[];
-    onClickAddCart?: VoidFunction;
     loading?: boolean;
-    // onSubmit: (itemId: number, ingredients: number[]) => void;
+    onSubmit: (itemId: number, ingredients: number[]) => void;
     className?: string;
 }
 
+/**
+ * Форма выбора ПИЦЦЫ
+ */
 export const ChoosePizzaForm: React.FC<Props> = ({
                                                      name,
                                                      items,
                                                      imageUrl,
                                                      ingredients,
-                                                     onClickAddCart,
                                                      loading,
-                                                     // onSubmit,
+                                                     onSubmit,
                                                      className,
                                                  }) => {
-
     const {
         size,
         type,
-        setType,
-        setSize,
         selectedIngredients,
+        availableSizes,
+        currentItemId,
+        setSize,
+        setType,
         addIngredient,
-        availableSizes
     } = usePizzaOptions(items);
 
     const { totalPrice, textDetails } = getPizzaDetails(
@@ -53,20 +55,17 @@ export const ChoosePizzaForm: React.FC<Props> = ({
     );
 
     const handleClickAdd = () => {
-        onClickAddCart?.();
-        console.log({
-            size,
-            type,
-            ingredients: selectedIngredients
-        })
-    }
+        if (currentItemId) {
+            onSubmit(currentItemId, Array.from(selectedIngredients));
+        }
+    };
 
     return (
         <div className={cn(className, 'flex flex-1')}>
-            <PizzaImage imageUrl={imageUrl} size={size}/>
+            <PizzaImage imageUrl={imageUrl} size={size} />
 
             <div className="w-[490px] bg-[#f7f6f5] p-7">
-                <Title text={name} size="md" className="font-extrabold mb-1"/>
+                <Title text={name} size="md" className="font-extrabold mb-1" />
 
                 <p className="text-gray-400">{textDetails}</p>
 
@@ -74,12 +73,13 @@ export const ChoosePizzaForm: React.FC<Props> = ({
                     <GroupVariants
                         items={availableSizes}
                         value={String(size)}
-                        onClick={value => setSize(Number(value) as PizzaSize)}
+                        onClick={(value) => setSize(Number(value) as PizzaSize)}
                     />
+
                     <GroupVariants
                         items={pizzaTypes}
                         value={String(type)}
-                        onClick={value => setType(Number(value) as PizzaType)}
+                        onClick={(value) => setType(Number(value) as PizzaType)}
                     />
                 </div>
 
@@ -99,8 +99,8 @@ export const ChoosePizzaForm: React.FC<Props> = ({
                 </div>
 
                 <Button
-                    onClick={handleClickAdd}
                     loading={loading}
+                    onClick={handleClickAdd}
                     className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
                     Добавить в корзину за {totalPrice} ₽
                 </Button>
