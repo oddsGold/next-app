bbf30cc3596551ad8b1f214a50ca1aef01f9fe88
Sulym Dmytro <ds.intelserv@gmail.com>
2025-03-13 18:44:12 +0100

    Test 4

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth"
 import {authOptions} from "@/shared/constants/auth-options";
 
-export const dynamic = "force-dynamic";
-
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -1,12 +1,11 @@
 import { prisma } from '@/prisma/prisma-client';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
     try {
-        const code = req.nextUrl.searchParams.get('code');
-        // const code = '';
+        // const code = req.nextUrl.searchParams.get('code');
+        const code = '';
 
         if (!code) {
             return NextResponse.json({ error: 'Неверный код' }, { status: 400 });
