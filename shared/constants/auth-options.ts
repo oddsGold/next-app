9c98cf18b15ad34eef9ad44035a786c5f4724e56
Sulym Dmytro <ds.intelserv@gmail.com>
2025-03-13 16:54:17 +0100

    Fixed bugs

--- shared/constants/auth-options.ts
+++ shared/constants/auth-options.ts
@@ -1,4 +1,4 @@
-import { AuthOptions } from 'next-auth';
+import {AuthOptions, NextAuthOptions} from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
