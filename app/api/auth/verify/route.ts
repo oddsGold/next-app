900a263ff85ec7d89cfacd092f61930d8059542f
Sulym Dmytro <ds.intelserv@gmail.com>
2025-03-14 09:27:59 +0100

    Test 8

--- app/api/auth/verify/route.ts
+++ app/api/auth/verify/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from '@/prisma/prisma-client';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const dynamic = 'force-dynamic';
+export const dynamic = 'force-dynamic'; 
 
 export async function GET(req: NextRequest) {
     try {
