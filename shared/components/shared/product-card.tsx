20a1602e6a7f3664bf2563f094fa30cc07bd5033
Sulym Dmytro <ds.intelserv@gmail.com>
2025-03-14 10:58:03 +0100

    Deleted $

--- shared/components/shared/product-card.tsx
+++ shared/components/shared/product-card.tsx
@@ -37,7 +37,7 @@ export const ProductCard: React.FC<Props> = ({
 
                 <div className="flex justify-between items-center mt-4">
                     <span className="text-[20px]">
-                        от <b>{price} $</b>
+                        от <b>{price}</b>
                     </span>
 
                     <Button variant="secondary" className="text-base font-bold">
