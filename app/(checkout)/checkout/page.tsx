208ee78f2a88df483f33042f298a2384577bdb1a
Sulym Dmytro <ds.intelserv@gmail.com>
2025-03-14 09:21:23 +0100

    Test 7

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -45,7 +45,7 @@ export default function CheckoutPage() {
         if (session) {
             fetchUserInfo();
         }
-    }, [session]);
+    }, [session, form]);
 
     const onSubmit = async (data: CheckoutFormValues) => {
         try {
